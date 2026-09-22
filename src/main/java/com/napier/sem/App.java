7497699197302c5ff9b9cd7d1e747321114d1d03
HtooAgKyw <htooaungkyaw2210@gmail.com>
2026-09-22 12:43:54 +0630

    Complete employee record information

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -70,9 +70,21 @@ public class App
 
             // Create string for SQL statement
             String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
+                    "SELECT e.emp_no, e.first_name, e.last_name, "
+                            + "t.title, s.salary, d.dept_name, "
+                            + "CONCAT(m.first_name, ' ', m.last_name) AS manager "
+                            + "FROM employees e "
+                            + "JOIN titles t ON e.emp_no = t.emp_no "
+                            + "JOIN salaries s ON e.emp_no = s.emp_no "
+                            + "JOIN dept_emp de ON e.emp_no = de.emp_no "
+                            + "JOIN departments d ON de.dept_no = d.dept_no "
+                            + "JOIN dept_manager dm ON d.dept_no = dm.dept_no "
+                            + "JOIN employees m ON dm.emp_no = m.emp_no "
+                            + "WHERE e.emp_no = " + ID + " "
+                            + "AND t.to_date = '9999-01-01' "
+                            + "AND s.to_date = '9999-01-01' "
+                            + "AND de.to_date = '9999-01-01' "
+                            + "AND dm.to_date = '9999-01-01'";
 
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
@@ -85,6 +97,10 @@ public class App
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
+                emp.title = rset.getString("title");
+                emp.salary = rset.getInt("salary");
+                emp.dept_name = rset.getString("dept_name");
+                emp.manager = rset.getString("manager");
 
                 return emp;
             }
